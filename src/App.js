3ff00aef8749ca3674a6d6ed324c06239e9693b3
Cihan Danisman <140907366+cihandanisman@users.noreply.github.com>
2024-04-16 22:08:52 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.scss';
 import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Projects from './pages/projects/Projects';
+import Contact from './pages/contact/Contact';
+
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
+      <Projects />
+      <Contact />
 
     </div>
   );
