11ffc56c25fd5f5b33001766c6a4d5c6b373886b
Cihan Danisman <140907366+cihandanisman@users.noreply.github.com>
2024-04-17 08:10:17 +0300

    new2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,26 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Navbar.scss"
+
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  const navigate = useNavigate();
-
+  
 
   return (
     <div>
       <nav class="nav">
         <ul class="nav__list">
-          <li onClick={() => navigate("/")} class="nav__item nav__link">
+          <Link to={'/'} class="nav__item nav__link">
             Home
-          </li>
-          <li
-            onClick={() => navigate("/about")}
-            class="nav__link nav__item"
-          >
+          </Link>
+          <Link to={"/about"} class="nav__link nav__item">
             About
-          </li>
-          <li onClick={() => navigate("/projects")} class="nav__item nav__link">
+          </Link>
+          <Link to={"/projects"}  class="nav__item nav__link">
             My Projects
-          </li>
-          <li onClick={() => navigate("/contact")} class="nav__item nav__link">
+          </Link>
+          <Link to={"/contact"} class="nav__item nav__link">
             Contact
-          </li>
+          </Link>
         </ul>
       </nav>
     </div>
